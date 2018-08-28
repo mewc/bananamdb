1269f7c1558e664210177b03c2c7ae38f2fdb9a7
Unknown <mikecarter771@gmail.com>
2018-08-28 17:09:11 +1000

    redux setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
+
+import {Provider} from 'react-redux';
+import { store } from './store/index';
+
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+render (
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
