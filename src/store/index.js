d2c41524e8676800ae29012dd470f5f97f0f83fc
Unknown <mikecarter771@gmail.com>
2018-08-29 10:23:59 +1000

    add redux devtools enhancer extension

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {createStore, combineReducers } from 'redux';
-
+import devToolsEnhancer from 'remote-redux-devtools';
 
 import * as reducers from '../reducers/reducers';
 
 export const store = createStore(
   combineReducers({
     state: reducers
-  })
+  }),
+  devToolsEnhancer()
 );
