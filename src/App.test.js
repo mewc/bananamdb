bdc8f4cfdb3773606788158b8e3bc4345031db7c
Unknown <mikecarter771@gmail.com>
2018-08-29 17:23:37 +1000

    test update

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {fetchMovies} from '../actions/actions.js';
+import {fetchMovies} from 'actions/actions.js';
 
 
 it('renders without crashing', () => {
